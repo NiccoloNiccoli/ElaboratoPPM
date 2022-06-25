38d762aec297d7b97264a14665e3a59c40c3f1ea
NiccoloNiccoli <niccolo.niccoli@stud.unifi.it>
2022-06-25 20:15:35 +0200

    Added new picture in database

--- artapplication.sql
+++ artapplication.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Creato il: Giu 23, 2022 alle 18:29
+-- Creato il: Giu 25, 2022 alle 18:14
 -- Versione del server: 5.7.36
 -- Versione PHP: 7.4.26
 
@@ -48,7 +48,8 @@ CREATE TABLE IF NOT EXISTS `art_images` (
 --
 
 INSERT INTO `art_images` (`name`, `author`, `location`, `year`, `low-res_link`, `low-res_width`, `mid-res_link`, `mid-res_width`, `high-res_link`, `high-res_width`) VALUES
-('Primavera', 'Sandro Botticelli', 'Galleria degli Uffizi, Firenze', 1480, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/640px-Botticelli-primavera.jpg', 640, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg', 1280, 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Botticelli-primavera.jpg', 4926);
+('Primavera', 'Sandro Botticelli', 'Galleria degli Uffizi, Firenze', 1480, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/640px-Botticelli-primavera.jpg', 640, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg', 1280, 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Botticelli-primavera.jpg', 4926),
+('Impression, soleil levant', 'Claude Monet', 'Musee Marmottan Monet, Parigi', 1872, 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Monet_-_Impression%2C_Sunrise.jpg/619px-Monet_-_Impression%2C_Sunrise.jpg', 619, 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Monet_-_Impression%2C_Sunrise.jpg/1280px-Monet_-_Impression%2C_Sunrise.jpg', 1280, 'https://upload.wikimedia.org/wikipedia/commons/5/59/Monet_-_Impression%2C_Sunrise.jpg', 5773);
 
 -- --------------------------------------------------------
 
@@ -66,14 +67,15 @@ CREATE TABLE IF NOT EXISTS `art_infos` (
   `question` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `art_infos`
 --
 
 INSERT INTO `art_infos` (`id`, `name`, `author`, `description`, `coordinates`, `question`) VALUES
-(1, 'Primavera', 'Sandro Botticelli', 'Lorem ipsum dolor sit amet.', '{\"top\": \"50%\", \"left\": \"4%\", \"right\": \"93%\", \"bottom\": \"10%\"}', 'Dove si trova Mercurio?');
+(1, 'Primavera', 'Sandro Botticelli', 'Lorem ipsum dolor sit amet.', '{\"top\": \"50%\", \"left\": \"4%\", \"right\": \"93%\", \"bottom\": \"10%\"}', 'Dove si trova Mercurio?'),
+(2, 'Impression, soleil levant', 'Claude Monet', 'Lorem ipsum dolor sit amet', '{\"top\": \"20%\", \"left\": \"55%\", \"right\": \"35%\", \"bottom\": \"55%\"}', 'Dove si trova il sole?');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
