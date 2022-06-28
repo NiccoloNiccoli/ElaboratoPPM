7faabff65ba2c78e06e08f2ba37982163ec18318
Nicco <niccolo.niccoli@stud.unifi.it>
2022-06-28 12:47:42 +0200

    Bugs fixed

--- scripts/jquery.artApplication.js
+++ scripts/jquery.artApplication.js
@@ -29,7 +29,8 @@
                     else if(width >= 2 * object['mr-width']){
                         imageLink = object['hr-link'];
                     }
-                    $('.imageBox').append("<img class = 'mainImg' src = " + imageLink + "><div class='areaToClick'></div>");
+                    $('#imgP1').append("<img class = 'mainImg' src = " + imageLink + "><div class='areaToClick' id='areaP1'></div>");
+                    $('#imgP2').append("<img class = 'mainImg' src = " + imageLink + "><div class='areaToClick' id='areaP2'></div>");
                     let coords = JSON.parse(object['coordinates']);
                     $('.areaToClick').css({'top' : coords['top'], 'right' : coords['right'], 'bottom' : coords['bottom'], 'left' : coords['left']});
                     const dataDesc = {"name" : object['name'], "author" : object['author'], "location" : object['location'], "year" : object['year'], "description" : object["description"], "image" : imageLink};
@@ -48,7 +49,7 @@
                 }
                 document.getElementById("points2").innerHTML = counter2.toString();
 
-                $('.areaToClick').on('click', function (event) {
+                /*$('.areaToClick').on('click', function (event) {
                     $('.areaToClick').css({'border': '3px solid green'});
                     let cursorX = event.pageX;
                     if (cursorX < window.innerWidth / 2){
@@ -77,6 +78,39 @@
                     }, 2000);
 
 
+                });*/
+                let haveAlreadyAnswered = false;
+                $('#areaP1').on('click', function (event) {
+                    if(!haveAlreadyAnswered){
+                        $('#areaP1').css({'border': '3px solid green'});
+                        let c1 = parseInt(localStorage.getItem("count1"));
+                        if(isNaN(c1)) {
+                            c1 = 0;
+                        }
+                        c1++;
+                        localStorage.setItem("count1", c1.toString());
+                        document.getElementById("points1").innerHTML = c1.toString();
+                        haveAlreadyAnswered = true;
+                        setTimeout(function(){
+                            window.location = "descriptionPage.html";
+                        }, 2000);
+                    }
+                });
+                $('#areaP2').on('click', function (event) {
+                    if(!haveAlreadyAnswered) {
+                        $('#areaP2').css({'border': '3px solid green'});
+                        let c2 = parseInt(localStorage.getItem("count2"));
+                        if (isNaN(c2)) {
+                            c2 = 0;
+                        }
+                        c2++;
+                        localStorage.setItem("count2", c2.toString());
+                        document.getElementById("points2").innerHTML = c2.toString();
+                        haveAlreadyAnswered = true;
+                        setTimeout(function () {
+                            window.location = "descriptionPage.html";
+                        }, 2000);
+                    }
                 });
             });
             request.fail(
