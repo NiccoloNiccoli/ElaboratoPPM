c0959d79760569b64b70c4ec57700915bcc9d9f4
SilviaDani <silvia.dani@stud.unifi.it>
2022-06-29 22:34:10 +0200

    Updated final page

---
+++ scripts/results.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    let counter1 = parseInt(localStorage.getItem("count1"));
+    if(isNaN(counter1)) {
+        counter1 = 0;
+    }
+    let counter2 = parseInt(localStorage.getItem("count2"));
+    if(isNaN(counter2)) {
+        counter1 = 0;
+    }
+    if(counter1>counter2){
+        document.getElementById("winner").innerHTML = "CONGRATULAZONI " + localStorage.getItem("player1") + " HAI VINTO!";
+    }
+    else if (counter2>counter1){
+        document.getElementById("winner").innerHTML = "CONGRATULAZONI " + localStorage.getItem("player2") + " HAI VINTO!";
+    }
+    else if(counter1 === counter2){
+         document.getElementById("winner").innerHTML = "PAREGGIO!";
+    }
+});
